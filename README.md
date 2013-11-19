6d27e4f9e0d1643860afa778614cb1069f04e38d
zhiyan <zhiyan1987@gmail.com>
2013-11-19 19:50:28 +0800

    init basic

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-jquery.notice
-=============
+# jQuery Notice
 
-a jquery plugin for public notice window
+A simple jQuery notice & popup plugin by ian
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/zhiyan/jquery.notice/master/dist/jquery.notice.min.js
+[max]: https://raw.github.com/zhiyan/jquery.notice/master/dist/jquery.notice.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jquery.notice.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.notice.js
@@ -0,0 +1,111 @@
+/*
+ * jquery.notice
+ * https://github.com/zhiyan/jquery.notice
+ *
+ * Copyright (c) 2013 ian
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  var $win = $(window),
+    $doc = $(document);
+
+  //notice class
+  function Notice(options) {
+    var html = "",
+      obj,
+      that = this,
+      container,
+      btnHref = "",
+      classWrap = "notice";
+    //provide overlap
+    if ($(".notice:visible").length) {
+      $(".notice").remove();
+    }
+    if (typeof options === "object"){
+      $.extend(this, options);
+    }
+    else if (typeof options === "string") {
+      this.content = options;
+    }
+    container = $(this.container);
+    classWrap += " " + this["class"] || "";
+    //template
+    html = '<div class="' + classWrap + '"><div class="inner-top"><h3>' + this.title + '</h3><div class="close"></div></div><div class="inner-middle">' + this.content + '</div><div class="inner-bottom"><div class="notice-btn">';
+    //callback
+    if ( !! this.buttonName && this.buttonDo) {
+      if (typeof this.buttonDo === "string"){
+        btnHref = this.buttonDo;
+      }
+      html += '<a href="' + btnHref + '" class="inner-btn button-do" style="margin-right:10px;">' + this.buttonName + '</a>';
+    }
+    if (!this.cancelhide){
+      html += '<a href="" class="inner-btn cancel">' + this.cancelName + '</a>';
+    }
+    html += '</div></div></div>';
+    //create obj
+    obj = $(html);
+    //button callback
+    if (typeof this.buttonDo === "function") {
+      obj.find(".button-do").click(function() {
+        that.buttonDo();
+        that.close(obj);
+        return false;
+      });
+    }
+    obj.find(".close,.cancel").click(function() {
+      that.close(obj);
+      if (typeof that.callback === "function") {
+        that.callback();
+      }
+      return false;
+    });
+    //free memory
+    html = null;
+    //add to page
+    container.append(obj);
+    //obj position
+    if (container[0] === document.body) {
+      that.makeCenter(obj);
+    }
+    //esc to quit
+    if (obj.find(".close,.cancel").length) {
+      $doc.bind("keydown", {
+        "obj": obj
+      }, that.escQuit);
+    }
+  }
+  Notice.prototype = {
+    "title": "Notice",
+    "content": "",
+    "container": document.body,
+    "cancelName": "Cancel",
+    close: function(obj) {
+      obj.remove();
+      $doc.unbind("keydown", this.escQuit);
+    },
+    makeCenter: function(obj) {
+      var w = parseInt(-obj.width()/2,10),
+          t = parseInt(-obj.height()/2,10);
+      obj.css({
+        "position": "absolute",
+        "left": "50%",
+        "top": $doc.scrollTop() + $win.height() / 2,
+        "margin-left" : w,
+        "margin-top" : t
+      });
+    },
+    escQuit: function(e) {
+      if (+e.keyCode === 27) {
+        e.data.obj.find(".close").trigger("click");
+      }
+    }
+  };
+
+  // Static method.
+  $.jquery_notice = function(options) {
+    return new Notice(options);
+  };
+
+}(jQuery));
